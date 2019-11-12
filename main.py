b30f5ba18fcc79000be64d8426a1898202a39a35
Alex <bodomus@gmail.com>
2019-11-12 17:26:53 +0200

    Update ver 1.0.0

--- main.py
+++ main.py
@@ -13,6 +13,7 @@ class CountryBoundaries():
         self.dataFile = dataFile
         self.countryName = countryName
         self.geo = None
+        print 'ver 1.0.0'
 
     def getCountryByName(self, items, name):
         for item in items:
@@ -22,6 +23,9 @@ class CountryBoundaries():
         return None
 
     def getGeometry(self, geo):
+        dict = {}
+        for index, item in enumerate (geo['coordinates']):
+            dict[str(index)] = item
         return geo['coordinates']
 
     def prn(self):
@@ -29,27 +33,31 @@ class CountryBoundaries():
             print(self.geo)
 
     def getCountryBoundaries(self):
+
         if (not os.path.isfile(self.dataFile)):
             return (None, "Invalid file path.")
         with open(self.dataFile) as json_file:
             data = json.load(json_file)
+
             p = data['features']
             type = data['type']
             if (type != "FeatureCollection"):
                 return (None, "Invalid format file")
             cName, cISO, cGeometry = self.getCountryByName(p, 'Ukraine')
-            self.geo = self.getGeometry(cGeometry)[0]
+            self.geo = self.getGeometry(cGeometry)
             return self.geo
             # for idx, point in enumerate(g):
             #    c = point
             #    print('index:%d %f %f ' % (idx, c[0], c[1]))
 
-    def createBoundaries(self):
-        line = hou.node('/obj').createNode('geo').createNode('line')
-        g = line.geometry()
-        for item in self.geo[0]:
-            p = g.createPoint()
-            p.setPosition(hou.Vector3(item[0], item[0], 0))
+    # def createBoundaries(self):
+    #     line = hou.node('/obj').createNode('geo').createNode('line')
+    #     g = line.geometry()
+    #     for item in self.geo:
+    #         for i in item:
+    #             for z in i:
+    #                 print z
+
         # line = hou.node('/obj').createNode('geo').createNode('line')
         # g = line.geometry().freeze()
         # p = g.points()[0]
@@ -81,9 +89,15 @@ if __name__ == "__main__":
     import json
     import hrpyc
 
-    con, hou = hrpyc.import_remote_module()
+    #con, hou = hrpyc.import_remote_module()
 
     boundaries = CountryBoundaries('countries.geojson', 'Ukraine')
-    boundaries.getCountryBoundaries()
-    boundaries.prn()
-    boundaries.createBoundaries()
+    list = boundaries.getCountryBoundaries()
+    #boundaries.prn()
+    for item in list:
+        for i in item:
+            print 'next'
+
+
+
+    #boundaries.createBoundaries()
